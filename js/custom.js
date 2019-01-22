0154591dad8263c962ae732b0be8ff0f98e83024
wishing4u <jitgurjar079@gmail.com>
2019-01-22 19:11:43 +0530

    updated
    dhfsdh

--- js/custom.js
+++ js/custom.js
@@ -15,6 +15,11 @@
 	   
 	});
 
+	let today = new Date().toISOString().substr(0, 10);
+document.querySelector("#today").value = today;
+
+document.querySelector("#today2").valueAsDate = new Date();
+
 
 	/*----------------------------------------------------*/
 	/*	Mobile Menu Toggle
